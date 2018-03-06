5162aca4a828eaa190788ea92325a5a5b9cb3770
Adam <mr_adam_man@hotmail.com>
2018-03-06 12:46:04 +0000

    Add files via upload

---
+++ fortnite.js
@@ -0,0 +1,29 @@
+document.querySelector("#btn").onclick = function () {
+
+    let inputVal = $("#input").val();
+    
+
+    $.ajax({
+        url: "https://fortnite.y3n.co/v2/player/" + inputVal,
+        dataType: "json",
+        headers: {
+            "X-key": "DoPtQoEN2CAzYfyHhqJW"
+        },
+        success: function (data) {
+            
+            console.log(data);
+            console.log(data.br.stats.ps4.all);
+            $("#data-div").animate({opacity:"1"}, 500);
+            $("#display-name").html("<img src='flag.png' class='flag'> " + data.displayName);
+            $("#kd").html("<span class='labels'>K/D:</span> " + data.br.stats.ps4.all.kpd);
+            
+            
+            
+            $("#kills").html("<span class='labels'>Kills:</span> " + data.br.stats.ps4.all.kills);
+            $("#deaths").html("<span class='labels'>Deaths:</span> " + data.br.stats.ps4.all.deaths);
+            $("#played").html("<span class='labels'>Matches Played:</span> " + data.br.stats.ps4.all.matchesPlayed);
+            $("#score").html("<span class='labels'>Score: </span>" + data.br.stats.ps4.all.score);
+        }
+    });
+}
+ 
