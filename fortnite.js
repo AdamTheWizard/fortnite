9f9357f39a894dad2e3f490962e4caf8fe025f96
Adam <mr_adam_man@hotmail.com>
2018-03-12 13:36:09 +0000

    Update fortnite.js

--- fortnite.js
+++ fortnite.js
@@ -1,7 +1,7 @@
 document.querySelector("#btn").onclick = function () {
 
     let inputVal = $("#input").val();
-    
+
 
     $.ajax({
         url: "https://fortnite.y3n.co/v2/player/" + inputVal,
@@ -10,20 +10,23 @@ document.querySelector("#btn").onclick = function () {
             "X-key": "DoPtQoEN2CAzYfyHhqJW"
         },
         success: function (data) {
-            
+
             console.log(data);
             console.log(data.br.stats.ps4.all);
-            $("#data-div").animate({opacity:"1"}, 500);
+            $("#data-div").animate({
+                opacity: "1"
+            }, 500);
             $("#display-name").html("<img src='flag.png' class='flag'> " + data.displayName);
             $("#kd").html("<span class='labels'>K/D:</span> " + data.br.stats.ps4.all.kpd);
-            
-            
-            
-            $("#kills").html("<span class='labels'>Kills:</span> " + data.br.stats.ps4.all.kills);
-            $("#deaths").html("<span class='labels'>Deaths:</span> " + data.br.stats.ps4.all.deaths);
-            $("#played").html("<span class='labels'>Matches Played:</span> " + data.br.stats.ps4.all.matchesPlayed);
+
+
+
+            $("#kills").html("<span class='labels'>Kills: </span> " + data.br.stats.ps4.all.kills);
+            $("#deaths").html("<span class='labels'>Deaths: </span> " + data.br.stats.ps4.all.deaths);
+            $("#played").html("<span class='labels'>Matches Played: </span> " + data.br.stats.ps4.all.matchesPlayed);
+            $("#won").html("<span class='labels'>Matches Won: </span>" + data.br.stats.ps4.all.wins);
             $("#score").html("<span class='labels'>Score: </span>" + data.br.stats.ps4.all.score);
+
         }
     });
 }
- 
